6a6e348bcbc1b465648e81cfaa9af00d0a3036e3
181847 <753090084@qq.com>
2017-12-04 20:33:21 +0800

    添加对PointerMath中的基本函数的测试。

--- Allocator/TestAllocator.cpp
+++ Allocator/TestAllocator.cpp
@@ -29,23 +29,43 @@ void TestUnit::AddTestUnit()
 		void* addr1 = reinterpret_cast<void *>(0x00000001);
 		void* addr2 = reinterpret_cast<void *>(0x1234567a);
 		
-		int success = 0;
+		int error = 0;
 
-		success += AddrEQ(0x00000002, PointerMath::alignForward(addr1, 2));
-		success += AddrEQ(0x00000004, PointerMath::alignForward(addr1, 4));
-		success += AddrEQ(0x00000008, PointerMath::alignForward(addr1, 8));
-		success += AddrEQ(0x00000010, PointerMath::alignForward(addr1, 16));
-		success += AddrEQ(0x00000020, PointerMath::alignForward(addr1, 32));
-		success += AddrEQ(0x1234567c, PointerMath::alignForward(addr2, 4));
-		success += AddrEQ(0x12345680, PointerMath::alignForward(addr2, 8));
-		success += AddrEQ(0x12345680, PointerMath::alignForward(addr2, 16));
-		success += AddrEQ(0x12345680, PointerMath::alignForward(addr2, 32));
-		return success == 9;
+		error += false == AddrEQ(0x00000002, PointerMath::alignForward(addr1, 2));
+		error += false == AddrEQ(0x00000004, PointerMath::alignForward(addr1, 4));
+		error += false == AddrEQ(0x00000008, PointerMath::alignForward(addr1, 8));
+		error += false == AddrEQ(0x00000010, PointerMath::alignForward(addr1, 16));
+		error += false == AddrEQ(0x00000020, PointerMath::alignForward(addr1, 32));
+		error += false == AddrEQ(0x1234567c, PointerMath::alignForward(addr2, 4));
+		error += false == AddrEQ(0x12345680, PointerMath::alignForward(addr2, 8));
+		error += false == AddrEQ(0x12345680, PointerMath::alignForward(addr2, 16));
+		error += false == AddrEQ(0x12345680, PointerMath::alignForward(addr2, 32));
+		return error  == 0;
 	TEST_UNIT_END;
 
 	TEST_UNIT_START("test alignForwardAdjustment")
+		void * addr1 = reinterpret_cast<void*>(0x04503302);
+		int error = 0;
 
-		return false;
+		error += false == EQ(0, PointerMath::alignForwardAdjustment(addr1, 2));
+		error += false == EQ(2, PointerMath::alignForwardAdjustment(addr1, 4));
+		error += false == EQ(6, PointerMath::alignForwardAdjustment(addr1, 8));
+		error += false == EQ(14, PointerMath::alignForwardAdjustment(addr1, 16));
+		error += false == EQ(30, PointerMath::alignForwardAdjustment(addr1, 32));
+
+		return error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test alignForwardAdjustment with header")
+		void * addr1 = reinterpret_cast<void*>(0x04503302);
+		int error = 0;
+
+		error += false == EQ(6, PointerMath::alignForwardAdjustmentWithHeader(addr1, 4, 4 ));
+		error += false == EQ(10, PointerMath::alignForwardAdjustmentWithHeader(addr1, 4, 8));
+		error += false == EQ(6, PointerMath::alignForwardAdjustmentWithHeader(addr1, 8, 4));
+		error += false == EQ(38, PointerMath::alignForwardAdjustmentWithHeader(addr1, 8, 32));
+		
+		return error == 0;
 	TEST_UNIT_END;
 }
 
