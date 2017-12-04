868bb3fba977dae7262dbbc15f9b7d87a2eac34d
181847 <753090084@qq.com>
2017-12-04 22:45:39 +0800

    测试一下LinearAllocator的基本功能。

--- Allocator/TestAllocator.cpp
+++ Allocator/TestAllocator.cpp
@@ -4,10 +4,29 @@
 #include "stdafx.h"
 #include "../../Library//MyTools/UnitTestModules.h"
 #include "../Allocators/PointerMath.h"
+#include "../Allocators/LinearAllocator.h"
+
+#pragma comment(lib, "Allocators.lib")
 
 DECLARE_TEST_UNITS;
 
 #define AddrEQ(u32, addr2) (reinterpret_cast<void*>(u32) == addr2)
+
+static const size_t gLinearSize = 256;
+static allocator::LinearAllocator gLinearAllocator(
+	gLinearSize,
+	malloc(gLinearSize));
+
+struct TestStruct
+{
+	int _a;
+	short _b;
+	long _c;
+	TestStruct(int a, short b, long c)
+		:_a(a), _b(b), _c(c) {}
+	TestStruct()
+		:_a(1), _b(2), _c(3) {}
+};
 
 void TestUnit::GetReady()
 {
@@ -22,7 +41,9 @@ void TestUnit::AfterTest()
 void TestUnit::AddTestUnit()
 {
 	TEST_UNIT_START("first unit startup")
-		return true;
+		int error = 0;
+		error += gLinearSize != gLinearAllocator.getSize();
+		return error == 0;
 	TEST_UNIT_END;
 
 	TEST_UNIT_START("test alignForward")
@@ -65,6 +86,34 @@ void TestUnit::AddTestUnit()
 		error += false == EQ(6, PointerMath::alignForwardAdjustmentWithHeader(addr1, 8, 4));
 		error += false == EQ(38, PointerMath::alignForwardAdjustmentWithHeader(addr1, 8, 32));
 		
+		return error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test malloc from gLinearAllocator")
+		DEBUG_MESSAGE("TestStruct size: %d\n", sizeof(TestStruct));
+		DEBUG_MESSAGE("TestStruct align: %d\n", alignof(TestStruct));
+		allocator::ALIGN_ARR_HEADER<TestStruct>::reType::ret;
+		int error = 0;
+		auto * shouldBeOne = allocator::AllocateNew<int>(gLinearAllocator);
+		*shouldBeOne = 1;
+		// after this allocating,
+		// next pointer must align with 0x4.
+
+		int restMemory = gLinearAllocator.getSize() - gLinearAllocator.getUsedMemory();
+		const size_t arrLength = 5;
+		auto * pTestStructArr = allocator::AllocateArray<TestStruct>(gLinearAllocator, arrLength);
+
+		error += 1 != *shouldBeOne;
+		error += 2 != gLinearAllocator.getNumAllocations();
+
+		for (int i = 0; i < arrLength; ++i)
+		{
+			error += NOT_EQ(1, pTestStructArr[i]._a);
+			error += NOT_EQ(2, pTestStructArr[i]._b);
+			error += NOT_EQ(3, pTestStructArr[i]._c);
+		}
+
+		allocator::showAllocator(gLinearAllocator);
 		return error == 0;
 	TEST_UNIT_END;
 }
