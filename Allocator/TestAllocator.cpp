8ff645156a0de4f61b3ef1f6975761a12565bdee
181847 <753090084@qq.com>
2017-12-05 19:32:45 +0800

    增加对LinearAllocator的测试，包括全部分配普通元素，数组元素，以及clear能否正常。作

--- Allocator/TestAllocator.cpp
+++ Allocator/TestAllocator.cpp
@@ -6,6 +6,7 @@
 #include "../Allocators/PointerMath.h"
 #include "../Allocators/LinearAllocator.h"
 #include "../Allocators/MemoryTracer.h"
+#include "../../Library/MyTools/Cleaner.h"
 
 #pragma comment(lib, "Allocators.lib")
 
@@ -90,24 +91,97 @@ void TestUnit::AddTestUnit()
 		return error == 0;
 	TEST_UNIT_END;
 
+	TEST_UNIT_START("test use out of the gLinearAllocator with char")
+		allocator::LinearAllocator linearAllocator(gLinearSize, malloc(gLinearSize));
+		Cleaner clearLinearAlc([&](){linearAllocator.clear();});
+		void * pstart = linearAllocator.getStart();
+		
+		for (int i = 0; i < gLinearSize; ++i)
+		{
+			allocator::AllocateNew<char>(linearAllocator);
+		}
+
+		int error = 0;
+
+		error += nullptr != linearAllocator.allocate(1, 1);
+		
+		return linearAllocator.getUsedMemory() == gLinearSize
+			&& error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("use out the LinearAllocator with array")
+		int error = 0;
+		allocator::LinearAllocator linearAllocator(gLinearSize, malloc(gLinearSize));
+		Cleaner clearLinearAlc([&]() {linearAllocator.clear(); });
+		void * pstart = linearAllocator.getStart();
+
+		const size_t headAdjustment = PointerMath::alignForwardAdjustment(pstart, alignof(u16))
+			+ PointerMath::ALIGN_ARR_HEADER<u16>::ret;
+		const size_t restMemory = gLinearSize - headAdjustment * sizeof(u16);
+
+		size_t maxLength = restMemory / sizeof(u16);
+		u16 * pArr = allocator::AllocateArray<u16>(linearAllocator, maxLength);
+
+		// this allocation should be success,after this allocation,
+		// any allocation will return nullptr.
+		error += pArr == nullptr;
+
+		error += nullptr != allocator::AllocateNew<u8>(linearAllocator);
+
+		return error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test clear LinearAllocator")
+		int error = 0;
+		allocator::LinearAllocator linearAllocator(gLinearSize, malloc(gLinearSize));
+		Cleaner clearLinearAlc([&]() {linearAllocator.clear(); });
+		void * pstart = linearAllocator.getStart();
+
+		// first allocate all the memory
+		for (int i = 0; i < gLinearSize; ++i)
+		{
+			allocator::AllocateNew<char>(linearAllocator);
+		}
+		// ensure that we cannot allocate any byte from the linearAllocator.
+		error += nullptr != linearAllocator.allocate(1, 1);
+	
+		// clear the linearAllocator
+		linearAllocator.clear();
+		
+		// mallocate all the memory with a array of u16.
+		const size_t	headAdjustment = PointerMath::alignForwardAdjustment(pstart, alignof(u16))
+											+ PointerMath::ALIGN_ARR_HEADER<u16>::ret;
+		const size_t	restMemory = gLinearSize - headAdjustment * sizeof(u16);
+		size_t			maxLength = restMemory / sizeof(u16);
+		u16 *			pArr = allocator::AllocateArray<u16>(linearAllocator, maxLength);
+
+		// dose the allocation success?
+		error += pArr == nullptr;
+
+		// and we will not get any other byte from the allocator.
+		error += nullptr != allocator::AllocateNew<u8>(linearAllocator);
+		
+		return error == 0;
+	TEST_UNIT_END;
+
 	TEST_UNIT_START("test malloc from gLinearAllocator")
 		DEBUG_MESSAGE("TestStruct size: %d\n", sizeof(TestStruct));
 		DEBUG_MESSAGE("TestStruct align: %d\n", alignof(TestStruct));
-
-		debug::debugAllocator::LinearMemoryTracer tracer(gLinearAllocator);
-		
+		allocator::LinearAllocator linearAllocator(gLinearSize, malloc(gLinearSize));
+		Cleaner clearLinearAlc([&]() {linearAllocator.clear(); });
+		debug::debugAllocator::LinearMemoryTracer tracer(linearAllocator);
 
 		int error = 0;
-		auto * shouldBeOne = allocator::AllocateNew<int>(gLinearAllocator);
+
+		auto * shouldBeOne = allocator::AllocateNew<int>(linearAllocator);
 		tracer.New<int>();
 		*shouldBeOne = 1;
-		error += tracer.report();
-		// after this allocating,
-		// next pointer must align with 0x4.
-		const size_t arrLength = 5;
-		auto * pTestStructArr = allocator::AllocateArray<TestStruct>(gLinearAllocator, arrLength);
-		tracer.NewArray<TestStruct>(arrLength);
 
+		error += tracer.report();
+
+		const size_t arrLength = 5;
+		auto * pTestStructArr = allocator::AllocateArray<TestStruct>(linearAllocator, arrLength);
+		tracer.NewArray<TestStruct>(arrLength);
 		for (int i = 0; i < arrLength; ++i)
 		{
 			error += NOT_EQ(1, pTestStructArr[i]._a);
@@ -115,12 +189,25 @@ void TestUnit::AddTestUnit()
 			error += NOT_EQ(3, pTestStructArr[i]._c);
 		}
 
-		allocator::AllocateNew<int>(gLinearAllocator);
+		const size_t longArrLength = 5;
+		auto plongArr = allocator::AllocateArray<unsigned long>(linearAllocator, longArrLength);
+		tracer.NewArray<unsigned long>(longArrLength);
 
 		error += 1 != *shouldBeOne;
 		error += tracer.report();
 
-		allocator::showAllocator(gLinearAllocator);
+		// WARNING this code will cause tracer dismatch the linearAllocator,
+		// because we want to test the tracer truly log the state
+		// of the allocator.
+		// so if corect, afther this operation,
+		// the trace should return a number > 0.
+		allocator::AllocateNew<int>(linearAllocator);
+
+		// if it return 0,
+		// there is some error in the tracer.
+		error += tracer.report() == 0;
+
+		//allocator::showAllocator(linearAllocator);
 		return error == 0;
 	TEST_UNIT_END;
 }
